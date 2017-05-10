2cd2030769b256e152808161fd41b0f6a474f679
Karun Chennuri <karun.chennuri@huawei.com>
2017-05-10 19:20:26 +0000

    cAdvisor client changes.
    changes made:
    * Code clean up & removed unnecessary logic.
    * simplified logic in computeNetworkUsage()
    * Removed hard-coding of returning canal cns

--- vendor/github.com/Huawei-PaaS/CNI-Genie/genie/genie-cadvisor-client.go
+++ vendor/github.com/Huawei-PaaS/CNI-Genie/genie/genie-cadvisor-client.go
@@ -12,29 +12,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/**
+This class enables cni-genie to check network usage interface wise on a node.
+In other words, it primarily computes network usage for each of the CNS on a
+given node i.e. what is the network usage of weave, flannel etc.
 
-// Ref: package genie https://github.com/google/cadvisor/blob/master/client/client.go
-
-
-// This is an implementation of a cAdvisor REST API in Go.
-// To use it, create a client (replace the URL with your actual cAdvisor REST endpoint):
-//   client, err := client.NewClient("http://192.168.59.103:8080/")
-// Then, the client interface exposes go methods corresponding to the REST endpoints.
-
+It returns CNS that has least load on it. So that, cni-genie can configure
+networking on the CNS with least load.
+ */
 package genie
 
 import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"math"
 	"net/http"
 	"path"
 	"strings"
 	"github.com/google/cadvisor/info/v1"
-	"github.com/golang/glog"
 	. "github.com/Huawei-PaaS/CNI-Genie/utils"
 	"os"
+	"bytes"
+
 )
 
 // Client represents the base URL for a cAdvisor client.
@@ -43,26 +42,10 @@ type Client struct {
 	httpClient *http.Client
 }
 
-// NewClient returns a new v1.3 client with the specified base URL.
-func NewClient(url string) (*Client, error) {
-	return newClient(url, http.DefaultClient)
-}
-
-func newClient(url string, client *http.Client) (*Client, error) {
-	if !strings.HasSuffix(url, "/") {
-		url += "/"
-	}
-	return &Client{
-		baseUrl:    fmt.Sprintf("%sapi/v1.3/", url),
-		httpClient: client,
-	}, nil
-}
-
 // Returns the JSON container information for the specified
 // Docker container and request.
 func GetDockerContainers(url string, query *v1.ContainerInfoRequest) (cinfo []ContainerStatsGenie, err error) {
 	u := containerInfoUrl(url, "/")
-	//ret := make(map[string]ContainerInfoGenie)
 	var containerInfoObj ContainerInfoGenie
 	if err = httpGetJsonData(&containerInfoObj, query, u, "get all containers info"); err != nil {
 		return
@@ -80,15 +63,15 @@ func httpGetJsonData(data, postData interface{}, url, infoName string) error {
 	var err error
 	fmt.Fprintf(os.Stderr, "CAdvisor Client Inside httpGetJsonData() = %v\n", data)
 	fmt.Fprintf(os.Stderr, "CAdvisor Client postData = %v\n", postData)
-	/*if postData != nil {
+	if postData != nil {
 		data, marshalErr := json.Marshal(postData)
 		if marshalErr != nil {
 			return fmt.Errorf("unable to marshal data: %v", marshalErr)
 		}
-		resp, err = client.httpClient.Post(url, "application/json", bytes.NewBuffer(data))
-	} else {*/
+		resp, err = http.DefaultClient.Post(url, "application/json", bytes.NewBuffer(data))
+	} else {
 		resp, err = http.DefaultClient.Get(url)
-	//}
+	}
 	fmt.Fprintf(os.Stderr, "CAdvisor Client resp = %v\n", resp)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "CAdvisor Client err = %v\n", err)
@@ -120,7 +103,7 @@ func httpGetJsonData(data, postData interface{}, url, infoName string) error {
 	return nil
 }
 
-// This is getting overcomplicated
+// This is getting overcomplicated. In future needs to be re-written.
 /**
 This method returns string array of network solutions with ascending order of downlink usage.
 eg: flannel=350, calico=250, weave=150
@@ -134,46 +117,30 @@ func computeNetworkUsage(cinfo []ContainerStatsGenie) (string) {
 	//go with flannel as first preference, calico as second
 	// weave as third
 	m := make(map[string]int)
-	m["weav"] 	= 1
-	m["cali"] 	= 2
-	m["flan"] 	= 3
-
+	m["flan"] 	= 0
+	m["cali"] 	= 0
+	m["weav"] 	= 0
+	var downlink int
 
 	rx := make(map[string]uint64)
-	//tx := make(map[string]uint64)
 
+	//TODO (Karun): Need to rethink on the logic. This is not an accurate measure.
 	for i, c := range cinfo {
-		use := []InterfaceBandwidthUsage{}
 		fmt.Fprintf(os.Stderr, "CAdvisor Client computeNetworkUsage i = %v\n", i)
 		for _, intf := range c.Network.Interfaces {
-			var downlink uint64
-			//var uplink uint64
 			if _, ok := m[intf.Name[:4]]; ok {
-				//fmt.Println("TxBytes=", intf.TxBytes)
-
 				if oldrx, ok := rx[intf.Name]; ok {
 					fmt.Fprintf(os.Stderr, "CAdvisor Client computeNetworkUsage intfname = %v\n", intf.Name[:4])
-					glog.V(6).Info("intfname=", intf.Name[:4])
-					glog.V(6).Info("RxBytes=", intf.RxBytes)
-					glog.V(6).Info("oldrx=", oldrx)
-					downlink = math.Float64bits(math.Abs(math.Float64frombits(intf.RxBytes) - math.Float64frombits(oldrx)))
+					fmt.Fprintf(os.Stderr, "CAdvisor Client computeNetworkUsage intf.RxBytes = %v\n", intf.RxBytes)
+					fmt.Fprintf(os.Stderr, "CAdvisor Client computeNetworkUsage oldrx = %v\n", oldrx)
+					downlink = int(intf.RxBytes - oldrx)
 				}
 				rx[intf.Name] = intf.RxBytes
-
-				/*if oldtx, ok := tx[intf.Name]; ok {
-					uplink = math.Float64bits(math.Abs(math.Float64frombits(intf.TxBytes) - math.Float64frombits(oldtx)))
-					tx[intf.Name] = intf.TxBytes
-				}*/
-				use = append(use,InterfaceBandwidthUsage{IntName: intf.Name, DownLink: downlink})
-				m[intf.Name[:4]] = int(downlink)
-				//use = append(use,InterfaceBandwidthUsage{IntName: intf.Name, DownLink: downlink, UpLink: uplink})
+				m[intf.Name[:4]] = downlink
 			}
 		}
-		fmt.Fprintf(os.Stderr, "CAdvisor Client computeNetworkUsage use = %v\n", use)
-		glog.V(6).Info("use==>", use)
 	}
 	fmt.Fprintf(os.Stderr, "CAdvisor Client computeNetworkUsage m = %v\n", m)
-	glog.V(6).Info("m==>", m)
 	//sort by values of map
 	cns := SortedKeys(m)
 	for i, c := range cns {
@@ -189,65 +156,26 @@ func computeNetworkUsage(cinfo []ContainerStatsGenie) (string) {
 		}
 	}
 	fmt.Fprintf(os.Stderr, "CAdvisor Client computeNetworkUsage cns = %v\n", cns[0])
-	fmt.Println("cns==>", cns)
 	return cns[0]
 }
 
 /**
-Returns array of strings with network solutions in descending order of network usage.
-input
-	- cAdvisorURL : http://127.0.0.1:4194 or http://<nodeip>:4194
+Returns network solution that has least load
+	- cAdvisorURL : http://127.0.0.1:4194 or http://<nodeip>:4194/api/v1.3
 	- numStats : int (number of stats needed default 3)
  */
-func GetCNSOrderByNetworkBandwith(cAdvisorURL string, numStats int) (string,error) {
+func GetCNSOrderByNetworkBandwith(cAdvisorURL string) (string,error) {
 	if cAdvisorURL == "" {
 		return "", fmt.Errorf("cAdvisorURL is empty. Should be eg: http://127.0.0.1:4194")
 	}
-	if numStats <= 0 {
-		numStats = 3
-	}
 
-	/*staticClient, err := NewClient(cAdvisorURL)
-	glog.V(6).Info("staticClient=", staticClient)
+	cinfo, err := GetDockerContainers(fmt.Sprintf("%s/api/v1.3/", cAdvisorURL), nil)
 	if err != nil {
-		glog.Errorf("tried to make client and got error %v", err)
-		return nil, err
-	}*/
-
-	//query := v1.ContainerInfoRequest{NumStats: numStats}
-	/*	query := v1.DefaultContainerInfoRequest()
-
-		fmt.Println("query==>", query)
-*/
-	cinfo, _ := GetDockerContainers(fmt.Sprintf("%s/api/v1.3/", cAdvisorURL), nil)
-	/*if err != nil {
-		glog.Errorf("got error retrieving event info: %v", err)
-		fmt.Errorf("****got error retrieving event info: %v", err)
-		return nil, err
-	}*/
+		fmt.Fprintf(os.Stderr, "CAdvisor Client cinfo err = %v\n", err)
+		return "", err
+	}
 	fmt.Fprintf(os.Stderr, "CAdvisor Client cinfo is = %v\n", cinfo)
-	//res := computeNetworkUsage(cinfo)
-
-	//res := []string{"canal", "weave", "test"}
-	res := "canal"
+	res := computeNetworkUsage(cinfo)
 	fmt.Fprintf(os.Stderr, "CAdvisor Client response = %v\n", res)
-	return res,nil
-
+	return res, nil
 }
-/*
-func staticContainersClientExample() {
-	cns,err := GetCNSOrderByNetworkBandwith("http://127.0.0.1:4194/", 3)
-	if err != nil {
-		glog.Errorf("got error while fetching CNS: %v", err)
-	}
-	glog.V(6).Info("cns=", cns)
-
-}
-
-
-// demonstrates how to use event clients
-func main() {
-	flag.Parse()
-	staticContainersClientExample()
-}
-*/
