28e7421eb3a62fe18b67f1b07e143ccffb4281c4
sushanthakumar <sushanthakumar@huawei.com>
2018-07-06 12:16:30 +0530

    Updated calico plugin installation link

--- plugins_install.sh
+++ plugins_install.sh
@@ -48,7 +48,7 @@ Install_Calico() {
   if [ "$status" == "$desiredState" ]; then
     echo "Calico already running"
   else
-    kubectl apply -f https://docs.projectcalico.org/v2.6/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
+    kubectl apply -f https://docs.projectcalico.org/v3.0/getting-started/kubernetes/installation/hosted/kubeadm/1.7/calico.yaml
     echo "Calico is started"
   fi
 }
